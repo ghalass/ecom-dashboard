8e897cdaeee13ed1e1bf114074fe9c1bf12032e2
MED SALEM GHALASS <msghalas@gmail.com>
2025-02-04 19:16:35 +0000

    menu-toggle

--- components/navigation/menu-toggle.tsx
+++ components/navigation/menu-toggle.tsx
@@ -1,5 +1,19 @@
-const MenuToggle = () => {
-  return <div>MenuToggle</div>;
-};
+"use client";
 
-export default MenuToggle;
+import { useMenuStore } from "@/store/toggleMenuStore";
+import { motion } from "framer-motion";
+import { ChevronsLeft, ChevronsRight } from "lucide-react";
+
+export default function MenuToggle() {
+  const { isOpen, toggleMenu } = useMenuStore();
+  return (
+    <button onClick={toggleMenu}>
+      <motion.div
+        animate={{ rotate: isOpen ? 360 : 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        {isOpen ? <ChevronsRight /> : <ChevronsLeft />}
+      </motion.div>
+    </button>
+  );
+}
